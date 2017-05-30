0c80954181fddf0dee1a14f8bdaafc82cddfb3cf
chun <1136389101@qq.com>
2017-05-31 01:51:29 +0800

    o

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,10 +6,47 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Hash;
+use DB;
 
 class UserController extends Controller
 {
+    public function getIndex(){
+        return view('admin.user.index');
+    }
+    /**
+     * 添加用户页面
+     */
     public function getAdd(){
         return view('admin.user.add');
     }
+
+    /**
+     * 添加用户
+     */
+   public function postInsert(Request $req){
+       //1. 表单验证
+       $this->validate($req, [
+           'username' => 'required|max:30',
+           'password' => 'required|max:30',
+           'repassword' => 'required|max:30|same:password',
+           'email' => 'required|email',
+       ], [
+           'username.required' => '用户名不能为空',
+           'username.max' => '用户名长度不能超过30',
+           'password.required' => '密码不能为空',
+           'password.max' => '密码长度不能超过30',
+           'repassword.required' => '确认密码不能为空',
+           'repassword.same' => '两次密码不一致',
+           'email.required' => '邮箱不能为空',
+           'email.email' => '邮箱格式不正确',
+       ]);
+       //2. 添加数据
+       $data = $req->except('_token', 'repassword');
+       $data['password'] = Hash::make($data['password']);
+       $data['token'] = str_random(32);
+
+       $res = DB::table('user')->insert($data);
+       return $res ? view('admin.user.index')->with('info', '添加成功') : back()->with('info', '添加失败');
+   }
 }
--- resources/views/admin/user/add.blade.php
+++ resources/views/admin/user/add.blade.php
@@ -20,44 +20,44 @@
 
             <div class="am-g tpl-amazeui-form">
 
-
                 <div class="am-u-sm-12 am-u-md-9">
-                    <form class="am-form am-form-horizontal">
+                    <form action="/admin/user/insert" method="post" class="am-form am-form-horizontal">
                         <div class="am-form-group">
                             <label for="user-name" class="am-u-sm-3 am-form-label">用户名</label>
                             <div class="am-u-sm-9">
-                                <input type="text" id="user-name" placeholder="用户名">
-                                <small>输入你的用户名，让我们记住你。</small>
+                                <input type="text" id="user-name" placeholder="用户名" name="username" value="{{old('username')}}">
+                                <small>{{$errors->first('username')}}</small>
                             </div>
                         </div>
 
                         <div class="am-form-group">
                             <label for="user-email" class="am-u-sm-3 am-form-label">密码</label>
                             <div class="am-u-sm-9">
-                                <input type="email" id="user-email" placeholder="输入你的密码">
-                                <small>密码你懂得...</small>
+                                <input type="password"  placeholder="输入你的密码" name="password" value="">
+                                <small>{{$errors->first('password')}}</small>
                             </div>
                         </div>
 
                         <div class="am-form-group">
                             <label for="user-phone" class="am-u-sm-3 am-form-label">确认密码</label>
                             <div class="am-u-sm-9">
-                                <input type="tel" id="user-phone" placeholder="输入确认密码">
-                                <small>确认下吧(*^__^*)</small>
+                                <input type="password" id="user-phone" placeholder="输入确认密码" name="repassword" value="">
+                                <small>{{$errors->first('repassword')}}</small>
                             </div>
                         </div>
 
                         <div class="am-form-group">
                             <label for="user-QQ" class="am-u-sm-3 am-form-label">邮箱</label>
                             <div class="am-u-sm-9">
-                                <input type="email" id="user-email" placeholder="输入你的邮箱">
-                                <small>邮箱哦,格式别写错了</small>
+                                <input type="email" id="user-email" placeholder="输入你的邮箱" name="email" value="{{old('email')}}">
+                                <small>{{$errors->first('email')}}</small>
                             </div>
                         </div>
 
                         <div class="am-form-group">
                             <div class="am-u-sm-9 am-u-sm-push-3">
-                                <button type="button" class="am-btn am-btn-primary">提交</button>
+                                {{csrf_field()}}
+                                <button type="submit" class="am-btn am-btn-primary">提交</button>
                             </div>
                         </div>
                     </form>
